f57bbd9150ff73d3cbc521c8f000a490ff7efbc3
root <root@gamewave.(none)>
2013-06-14 10:25:40 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'bc5c14a46250fd3b545dfeb1ec6ff993ac282d93081a96615cacbcb140502e9d0563bbece450e5d4694f90b3f7234ba87f17571eefd7cc4036530434669a9cd5'
